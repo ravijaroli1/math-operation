fcf6652f8b2af288f69be266546715f6e6ddd6d4
Ravi J <ravi.j@testyantra.com>
2019-02-15 15:29:30 +0530

    addition,subtraction,multiplication,division function added

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-module.exports = function(width, height) {
-  return width * height;
+var exports = module.exports;
+exports.multiplication = function(num1, num2) {
+  return num1 * num2;
+};
+
+exports.addition = function(num1, num2) {
+  return num1 + num2;
+};
+
+exports.subtraction = function(num1, num2) {
+  return num1 - num2;
+};
+
+exports.division = function(num1, num2) {
+  return num1 / num2;
 };
